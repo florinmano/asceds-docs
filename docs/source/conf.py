9b8c8e8b3af818ffcbb6dc55ece79b0b7e6e648b
Shankara Narayana <shankaranarayana01061997@gmail.com>
2021-12-07 09:16:04 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ASCEDS'
+copyright = '2021, CMU'
+author = 'Florin Manlache'
 
 release = '0.1'
 version = '0.1.0'
